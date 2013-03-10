ca7d513bbc7c85ce245c548108d6a91a0b0a8b0b
Jnani Weibel <jnaneshwar1234@gmail.com>
2013-03-10 11:34:38 -0700

    Added SQLite and many changes to the project

---
+++ src/com/TeamNovus/Persistence/Annotations/Columns/Column.java
@@ -0,0 +1,17 @@
+package com.TeamNovus.Persistence.Annotations.Columns;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+public @interface Column {
+	
+	String name();
+	boolean unique() default false;
+	boolean notNull() default false;
+	int length() default 0;
+	
+}
